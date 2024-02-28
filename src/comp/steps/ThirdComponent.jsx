c2dbc1fbfcf12a18533b79b7529fa9d605fe18db
bannu773 <bandibannu773@gmail.com>
2024-02-28 22:56:51 +0530

    Updated

--- src/comp/steps/ThirdComponent.jsx
+++ src/comp/steps/ThirdComponent.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const Declaration = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
-      saepe 
+    <div style={{ lineHeight: "30px" ,textAlign:"left",padding:"40px"}} >
+      Star Finance is one of the most trusted entity in Small and Medium Banking & Finance segment, and as part its recent modernization drive, its planned to digitalize its product portfolio to reach wide segment of peoples across india. At present these functional offerings are being carried our manually across their branch in their main cities
+      Core Functional Offerings
+      <br />
+      -	Gold / Silver Loan (Phase 1)<br />
+      -	Loan against Properties (Phase 2) <br />
+      -	Loan against Automobiles (Phase 3) <br />
+      As part of phase 1, its planned to automate & web enable the “Gold & Silver Loan” application business.
+
     </div>
   );
 };
